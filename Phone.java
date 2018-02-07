136764c230f429fc57acc8c3a8041f2fbfb25db1
zabdiel98 <zab.esc22@outlook.com>
2018-02-07 13:12:43 -0500

    First commit

---
+++ Phone.java
@@ -0,0 +1,13 @@
+
+public class Phone {
+	String brand;
+	String name;
+	int year;
+	double price;
+	
+public Phone(String brand, String name, int year, double price) {
+	this.brand=brand;
+	this.name=name;
+	this.year=year;
+	this.price=price;
+}}
